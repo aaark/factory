b7fb70fca30b3922bf095292dbbc0ae77f9445a4
rahul <rahul.ojha@forgeahead.io>
2016-08-10 10:04:29 +0530

    deleted unusual code from migration

--- db/migrate/20160808054541_change_auto_increment.rb
+++ db/migrate/20160808054541_change_auto_increment.rb
@@ -1,7 +1,6 @@
 class ChangeAutoIncrement < ActiveRecord::Migration[5.0]
  def self.up
-    execute "ALTER TABLE vehicles AUTO_INCREMENT=1001;
-"
+    execute "ALTER TABLE vehicles AUTO_INCREMENT=1001;"
   end
 
 end
